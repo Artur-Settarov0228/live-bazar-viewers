c3b81ad2ce466e6e46b7b6f373726be2fdca1e3a
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-11-09 04:05:48 +0500

    support qisim bajarildi

--- livebazar/urls.py
+++ livebazar/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, support
 
 urlpatterns = [
-    path("home/", home, name="home")
+    path("home/", home, name="home"),
+    path("support/", support , name="support")
 ]
--- livebazar/views.py
+++ livebazar/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("TG_API")
+CHAT_ID = os.getenv("USER_ID")
 
 def home(request: HttpRequest)->HttpResponse:
     return render(request=request, template_name="index.html")
+
+def support(request:HttpRequest)->HttpResponse:
+    if request.method == "POST":
+        name = request.POST.get("ism")
+        telefon = request.POST.get("telefon")
+        message = request.POST.get("xabar")
+
+
+        user_message = f"👤: {name} \n 📱:{telefon} \n 📩:{message}"
+
+
+        urls = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": user_message
+        }
+
+        requests.post(url=urls, params=payload)
+
+        return render(request=request, template_name="support.html")
+    
+    return render(request=request, template_name="support.html")
+
+
+   
